86aadc550242aba20156f319c59584c044873971
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-08 01:42:06 -0300

    Revert "Merge pull request #53 from tryber/inventory-report-ana-felipe-req1"
    This reverts commit f90132c2d5d834c4c5d8a3f0fc336830cdc71462, reversing
    changes made to 1cfeac4c1ca5bc9006cd71b5830908b01a90943a.

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,31 +1,10 @@
 import json
-from datetime import date, datetime
-
-DATE_FORMAT = "%Y-%m-%d"
 
 
 class SimpleReport:
     @staticmethod
     def generate(data):
-        manufacture_dates = []
-        expirations = []
-        companys = []
-
-        today = date.today()
-
-        for row in data:
-            manufacture_dates.append(row["data_de_fabricacao"])
-            if row["data_de_validade"] > datetime.strftime(today, DATE_FORMAT):
-                expirations.append(row["data_de_validade"])
-            companys.append(row["nome_da_empresa"])
-
-        comp = max(companys)  # pegando a empresa que mais se repete
-
-        return (
-            f"Data de fabricação mais antiga: {min(manufacture_dates)}\n"
-            f"Data de validade mais próxima: {min(expirations)}\n"
-            f"Empresa com maior quantidade de produtos estocados: {(comp)}\n"
-        )
+        pass
 
 
 if __name__ == "__main__":
@@ -36,5 +15,4 @@ if __name__ == "__main__":
             return [value for value in inventory_list]
 
 
-# data = read_file("../data/inventory.json")
-# print(SimpleReport.generate(data))
+print(read_file("../data/inventory.json"))
