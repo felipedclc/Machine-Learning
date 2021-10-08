2c9cbbb2bb7345daa8ef0a48a09aaa6f260d0afb
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-08 01:37:48 -0300

    reqs 3 e 4

---
+++ inventory_report/inventory/inventory.py
@@ -0,0 +1,21 @@
+from inventory_report.reports.simple_report import SimpleReport
+import csv
+import json
+
+
+class Inventory:
+    @classmethod
+    def import_data(cls, filePath, reportType):
+        fileData = cls.get_data(filePath)
+        if reportType == "simples":
+            return SimpleReport.generate(fileData)
+
+    @classmethod
+    def get_data(cls, filePath):
+        with open(filePath) as file:
+            if ".csv" in filePath:
+                content = csv.DictReader(file)
+                return list(content)
+            elif ".json" in filePath:
+                content = json.load(file)
+                return content
