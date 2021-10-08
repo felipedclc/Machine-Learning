6b1a35347c09c8cb013a538bbbd85bc0bc201af5
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-08 02:14:38 -0300

    refactoring

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -6,17 +6,18 @@ import json
 
 class Inventory:
     @classmethod
-    def import_data(cls, filePath, reportType):
-        fileData = cls.get_data(filePath)
-        if reportType == "simples":
-            return SimpleReport.generate(fileData)
+    def import_data(cls, file_path, report_type):
+        file_data = cls.get_data(file_path)
+        if report_type == "simples":
+            return SimpleReport.generate(file_data)
 
     @classmethod
-    def get_data(cls, filePath):
-        with open(filePath) as file:
-            if ".csv" in filePath:
+    def get_data(cls, file_path):
+        with open(file_path) as file:
+            if ".csv" in file_path:
                 content = csv.DictReader(file)
                 return list(content)
-            elif ".json" in filePath:
+            elif ".json" in file_path:
                 content = json.load(file)
                 return content
+            # elif ".xml" in file_path:
