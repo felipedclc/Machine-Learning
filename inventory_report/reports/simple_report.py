182dbc09c392e7a5b5e39f8812807f4f1ab647d2
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-08 01:52:36 -0300

    pulling from main branch

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,10 +1,31 @@
 import json
+from datetime import date, datetime
+
+DATE_FORMAT = "%Y-%m-%d"
 
 
 class SimpleReport:
     @staticmethod
     def generate(data):
-        pass
+        manufacture_dates = []
+        expirations = []
+        companys = []
+
+        today = date.today()
+
+        for row in data:
+            manufacture_dates.append(row["data_de_fabricacao"])
+            if row["data_de_validade"] > datetime.strftime(today, DATE_FORMAT):
+                expirations.append(row["data_de_validade"])
+            companys.append(row["nome_da_empresa"])
+
+        comp = max(companys)  # pegando a empresa que mais se repete
+
+        return (
+            f"Data de fabricação mais antiga: {min(manufacture_dates)}\n"
+            f"Data de validade mais próxima: {min(expirations)}\n"
+            f"Empresa com maior quantidade de produtos estocados: {(comp)}\n"
+        )
 
 
 if __name__ == "__main__":
@@ -15,4 +36,5 @@ if __name__ == "__main__":
             return [value for value in inventory_list]
 
 
-print(read_file("../data/inventory.json"))
+# data = read_file("../data/inventory.json")
+# print(SimpleReport.generate(data))
