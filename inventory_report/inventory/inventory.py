84a16520d569824f749be67a5c919e1c9d577fb1
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-09 01:01:00 -0300

    req 5

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,7 +1,9 @@
 
 from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 class Inventory:
@@ -10,6 +12,8 @@ class Inventory:
         file_data = cls.get_data(file_path)
         if report_type == "simples":
             return SimpleReport.generate(file_data)
+        if report_type == "completo":
+            return CompleteReport.generate(file_data)
 
     @classmethod
     def get_data(cls, file_path):
@@ -20,4 +24,10 @@ class Inventory:
             elif ".json" in file_path:
                 content = json.load(file)
                 return content
-            # elif ".xml" in file_path:
+            elif ".xml" in file_path:
+                doc = xmltodict.parse(file.read())
+                content = [
+                    dict(element) for element in doc['dataset']['record']
+                ]
+                return content
+                # https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
