f8f3471377f88839018422092391e739877ee0a1
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2021-10-09 02:19:38 -0300

    req 6

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,9 +1,9 @@
 
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-import csv
-import json
-import xmltodict
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
@@ -17,17 +17,9 @@ class Inventory:
 
     @classmethod
     def get_data(cls, file_path):
-        with open(file_path) as file:
-            if ".csv" in file_path:
-                content = csv.DictReader(file)
-                return list(content)
-            elif ".json" in file_path:
-                content = json.load(file)
-                return content
-            elif ".xml" in file_path:
-                doc = xmltodict.parse(file.read())
-                content = [
-                    dict(element) for element in doc['dataset']['record']
-                ]
-                return content
-                # https://python-guide-pt-br.readthedocs.io/pt_BR/latest/scenarios/xml.html
+        if ".csv" in file_path:
+            return CsvImporter.import_data(file_path)
+        elif ".json" in file_path:
+            return JsonImporter.import_data(file_path)
+        elif ".xml" in file_path:
+            return XmlImporter.import_data(file_path)
